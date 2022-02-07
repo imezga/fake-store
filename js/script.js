81b63e0b6fbd1170177ebadee9c737f5f1df9598
imezga <mezgaivan5@gmail.com>
2022-02-07 17:37:20 +0100

    Layout branch: responsive layout finished, ready to merge with main branch

--- js/script.js
+++ js/script.js
@@ -4,4 +4,5 @@ document.addEventListener('DOMContentLoaded', function () {
             .then((response) => response.json())
             .then((data) => console.log(data));
     }
+    getProducts();
 });
